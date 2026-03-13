b3da99341b220b5d468ced23c7891a23e61ad2aa
Pablo Esteban <termigol82@gmail.com>
2026-03-13 13:24:05 +0100

    pre deploy

--- src/Login.jsx
+++ src/Login.jsx
@@ -63,6 +63,7 @@ export default function Login({ dark, setDark, onLogin }) {
             placeholder="Email administrador"
             value={email}
             onChange={e => setEmail(e.target.value)}
+            autoComplete="username"
             required
           />
 
@@ -71,6 +72,7 @@ export default function Login({ dark, setDark, onLogin }) {
             placeholder="Contraseña"
             value={password}
             onChange={e => setPassword(e.target.value)}
+            autoComplete="current-password"
             required
           />
 
